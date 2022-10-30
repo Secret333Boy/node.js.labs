b6c792cd053f9ecadba8baf6246965bcbb84cbb1
GluterusMaximus <supkar1756@gmail.com>
2022-10-30 16:13:18 +0200

    Made requested changes

--- src/lib/Router.ts
+++ src/lib/Router.ts
@@ -1,29 +1,50 @@
+/* eslint-disable no-unused-vars */
 import { IncomingMessage, ServerResponse } from 'http';
 import { URL } from 'url';
 import send from './send.js';
 
-type HTTP_METHOD =
-  | 'GET'
-  | 'POST'
-  | 'PUT'
-  | 'DELETE'
-  | 'PATCH'
-  | 'OPTIONS'
-  | 'HEAD'
-  | 'CONNECT'
-  | 'TRACE';
+export enum HTTP_METHODS {
+  Get = 'get',
+  Post = 'post',
+  Put = 'put',
+  Delete = 'delete',
+  Patch = 'patch',
+  Options = 'options',
+  Head = 'head',
+  Connect = 'connect',
+  Trace = 'trace',
+}
 
-// eslint-disable-next-line no-unused-vars
-type Handler = (req: IncomingMessage, res: ServerResponse) => void;
+type Handler = (
+  req: IncomingMessage,
+  res: ServerResponse
+) => void | Promise<void>;
+type MethodHandlers = (path: string, ...handlers: Handler[]) => void;
 
 export default class Router {
   private handlers: { [path: string]: { [method: string]: Handler[] } } = {};
+  [HTTP_METHODS.Get]: MethodHandlers;
+  [HTTP_METHODS.Post]: MethodHandlers;
+  [HTTP_METHODS.Put]: MethodHandlers;
+  [HTTP_METHODS.Delete]: MethodHandlers;
+  [HTTP_METHODS.Options]: MethodHandlers;
+  [HTTP_METHODS.Trace]: MethodHandlers;
+  [HTTP_METHODS.Head]: MethodHandlers;
+  [HTTP_METHODS.Connect]: MethodHandlers;
+  [HTTP_METHODS.Patch]: MethodHandlers;
+
+  constructor() {
+    for (const method of Object.values(HTTP_METHODS)) {
+      this[method] = this.add.bind(this, method);
+    }
+  }
 
   handle(req: IncomingMessage, res: ServerResponse): void {
     const path = new URL(req.url || '/', `http://${req.headers.host}`).pathname;
-    const method = req.method || 'GET';
+    const method = req.method?.toLowerCase() || HTTP_METHODS.Get;
 
     const routeHandlers = this.handlers[path]?.[method];
+    console.dir({ path, method });
     if (!routeHandlers || routeHandlers.length === 0) {
       send(res, 'Not found', 'json', 404);
       return;
@@ -34,7 +55,7 @@ export default class Router {
     }
   }
 
-  add(method: HTTP_METHOD, path = '/', ...handlers: Handler[]) {
+  add(method: HTTP_METHODS, path = '/', ...handlers: Handler[]) {
     if (!this.handlers[path]?.[method])
       this.handlers[path] = {
         ...(this.handlers[path] || {}),
